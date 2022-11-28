9680970677bd27bfaf81b268c8c1cb96ad6f171d
damp cow <ovesamaki@gmail.com>
2022-11-28 13:08:42 +0200

    dragon deez nuts

---
+++ script.js
@@ -0,0 +1,32 @@
+var licks = 0;
+var lickerPrice = 10;
+var lickers = 0;
+var tickRate = 1000;
+
+var intervalID = window.setInterval(licker, tickRate);
+
+function lick() {
+    licks++;
+    if (licks == 1) {
+        document.getElementById("lick-amount").innerHTML = `You have licked ${licks} time`;
+    } else {
+        document.getElementById("lick-amount").innerHTML = `You have licked ${licks} times`;
+    }
+}
+
+function buyLicker() {
+    if (licks >= lickerPrice) {
+        licks -= lickerPrice;
+        lickerPrice += 5;
+        lickers++;
+        document.getElementById("licker-price").innerHTML = `Buy auto (c)licker! (${lickerPrice} licks)`;
+        document.getElementById("lick-amount").innerHTML = `You have licked ${licks} times  `;
+    }
+}
+
+function licker() {
+    if (lickers >= 1) {
+        licks += lickers;
+        document.getElementById("lick-amount").innerHTML = `You have licked ${licks} times  `;
+    }
+}
